f88e90494bc8f4796eb352f47d9b40d6161f26e0
Aaron Bedward <abedward@aprilavenue.co.uk>
2023-03-29 23:26:10 +0100

    remove  content

--- pages/addDocument.tsx
+++ pages/addDocument.tsx
@@ -12,13 +12,6 @@ import { UserDetails, Subscription } from 'types';
 import { NextRouter, useRouter } from 'next/router';
 import { v4 as uuidv4 } from 'uuid';
 
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-import { OpenAIEmbeddings } from 'langchain/embeddings';
-import { PineconeStore } from 'langchain/vectorstores';
-import { pinecone } from '@/utils/pinecone-client';
-import { CustomPDFLoader } from '@/utils/customPDFLoader';
-import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
-import { DirectoryLoader } from 'langchain/document_loaders';
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const supabase = createServerSupabaseClient(ctx);
@@ -122,43 +115,5 @@ const uploadPhoto = async (
     }
   } else {
     console.error('Upload failed.');
-  }
-};
-
-const inject = async (file: File) => {
-  try {
-    /*load raw docs from the all files in the directory */
-    //  const directoryLoader = new DirectoryLoader(filePath, {
-    // '.pdf': (path) =>
-    // new CustomPDFLoader(path),
-    //  });
-
-    const loader = new CustomPDFLoader(file);
-    // const rawDocs = await directoryLoader.load();
-    const rawDocs = await loader.load();
-
-    /* Split text into chunks */
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 200
-    });
-
-    const docs = await textSplitter.splitDocuments(rawDocs);
-    console.log('split docs', docs);
-
-    console.log('creating vector store...');
-    /*create and store the embeddings in the vectorStore*/
-    const embeddings = new OpenAIEmbeddings();
-    const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
-
-    //embed the PDF documents
-    await PineconeStore.fromDocuments(docs, embeddings, {
-      pineconeIndex: index,
-      namespace: PINECONE_NAME_SPACE,
-      textKey: 'text'
-    });
-  } catch (error) {
-    console.log('error', error);
-    throw new Error('Failed to ingest your data');
   }
 };
