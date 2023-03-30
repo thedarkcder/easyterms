b71ad75d71b3febaa17438b5210ed0c14347e722
Aaron Bedward <abedward@aprilavenue.co.uk>
2023-03-30 15:18:36 +0100

    add cresds

--- pages/api/upload-url.ts
+++ pages/api/upload-url.ts
@@ -5,17 +5,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-
-
-  console.log('s:', process.env.AWS_ACCESS_KEY_ID );
-  console.log('d:', process.env.AWS_SECRET_ACCESS_KEY);
-  console.log('PINECONE_INDEX_NAME:', process.env.PINECONE_INDEX_NAME );
-  console.log('SUPABASE_SERVICE_ROLE_KEY:', process.env.SUPABASE_SERVICE_ROLE_KEY );
-
-
-  
   const s3 = new S3({
     apiVersion: '2006-03-01',
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env?.AWS_ACCESS_KEY_ID + '',
+      secretAccessKey: process.env?.AWS_SECRET_ACCESS_KEY + ''
+    }
   });
 
   const post = await s3.createPresignedPost({
